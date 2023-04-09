ef2afc1f92780a488b526e779af8618738a0219a
Rain <yainwrd@gmail.com>
2023-04-09 20:07:52 +1000

    quiz name edited

--- frontend/src/components/buttons/CreateGame.jsx
+++ frontend/src/components/buttons/CreateGame.jsx
@@ -1,5 +1,5 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import VideogameAssetIcon from '@mui/icons-material/VideogameAsset';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
