68f815285b75216f5830806e3690945b6e988d05
Gatsbyz_sc <zhousichen06@gmail.com>
2023-04-16 04:20:00 +1000

    2.3.3

--- frontend/src/components/SessionResults.jsx
+++ frontend/src/components/SessionResults.jsx
@@ -1,5 +1,21 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { fetchGET } from '../library/fetch.js'
 
 export default function SessionResult () {
+  const sessionId = 299346
+  const [sessionResult, setSessionResult] = useState([])
+  const [sessionQuestion, setSessionQuestion] = useState([])
+  console.log(sessionResult)
+  console.log(sessionQuestion)
+  useEffect(async () => {
+    const sessionResults = (await fetchGET(`admin/session/${sessionId}/results`, 'token')).results[0]
+    setSessionResult(sessionResults)
+  }, [])
+
+  useEffect(async () => {
+    const sessionQuestions = (await fetchGET(`admin/session/${sessionId}/status`, 'token')).results.questions
+    setSessionQuestion(sessionQuestions)
+  }, [])
+
   return (<></>);
 }
